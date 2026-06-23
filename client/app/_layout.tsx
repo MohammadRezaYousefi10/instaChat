c1ca3f09f2ec6141dfa44f14850ff9ae10a704e3
MohammadRezaYousefi10 <mmdaghareza5@gmail.com>
2026-06-23 15:56:38 +0330

    compeleted message list screen

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,11 +1,12 @@
 import { Redirect, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 
 
 function AuthGuard(){
-  const {isSignedIn} = {isSignedIn : false}
+  const {isSignedIn} = {isSignedIn : true}
 
   if(!isSignedIn) {
     return <Redirect href='/(auth)'/>
