15b84baa50b715fa604f450b23505a46e289372a
MohammadRezaYousefi10 <mmdaghareza5@gmail.com>
2026-06-29 15:48:50 +0330

    backend completed for chat app

---
+++ server/routes/storyRoutes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import upload from "../middlewares/upload.js";
+import { createStory, getStories } from "../controllers/storyController.js";
+import { authMiddleware } from "../middlewares/auth.js";
+
+
+const storyRouter = Router()
+
+storyRouter.use(authMiddleware)
+
+storyRouter.post('/' , upload.single("file") , createStory)
+
+storyRouter.get('/' , getStories)
+
+export default storyRouter;
