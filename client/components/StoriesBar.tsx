6921ff2fc5c9c3774503495d5bdde82109d7e307
MohammadRezaYousefi10 <mmdaghareza5@gmail.com>
2026-06-30 16:22:12 +0330

    completed chat app

--- client/components/StoriesBar.tsx
+++ client/components/StoriesBar.tsx
@@ -1,12 +1,12 @@
 import { View, Text, FlatList, TouchableOpacity, Alert } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { styles } from '@/assets/styles/StoriesBar.styles'
 import { UserStory } from '@/types'
-import { dummyStoriesData } from '@/assets/assets'
 import { Ionicons } from '@expo/vector-icons'
 import { Colors } from '@/constants/Colors'
 import * as ImagePicker from 'expo-image-picker'
 import Avatar from './Avatar'
+import { api, useApp } from '@/context/AppContext'
 
 interface StoriesBarProbs {
     onViewStoty : (us : UserStory) => void
@@ -15,7 +15,11 @@ interface StoriesBarProbs {
 export default function StoriesBar({onViewStoty} : StoriesBarProbs) {
 
     const [uploading , setUploading] = useState(false);
-    const {userStories} = {userStories : dummyStoriesData}
+    const {userStories , fetchStories} = useApp();
+
+    useEffect(() => {
+        fetchStories()
+    },[fetchStories])
 
     const pickAndUpload = async () => {
         const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -41,9 +45,17 @@ export default function StoriesBar({onViewStoty} : StoriesBarProbs) {
         } as any)
 
         setUploading(true)
-        setTimeout(() => {
-           setUploading(false) 
-        }, 2000);
+        try {
+            const {data} = await api.post("/api/stories" , formData ,{
+                headers: {"Content-Type" : "multipart/form-data"}
+            })
+            if(data.success) fetchStories()
+        } catch (error : any) {
+            Alert.alert("Error" , "Failed to post story");
+            console.log(error)
+        }finally{
+            setUploading(false)
+        }
     }
 
   return (
--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -6,12 +6,14 @@ import { authMiddleware } from "../middlewares/auth.js";
 
 const userRouter = Router();
 
+userRouter.use(authMiddleware)
+
 userRouter.get('/' , getUsers)
 
 userRouter.get('/search' , searchUsers)
 
-userRouter.get('/profile' ,authMiddleware , getProfile)
+userRouter.get('/profile'  , getProfile)
 
-userRouter.put('/profile' , upload.single("avatar") , authMiddleware, updateProfile)
+userRouter.put('/profile' , upload.single("avatar") , updateProfile)
 
 export default userRouter
